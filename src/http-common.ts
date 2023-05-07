df54d593fdbe9766bc10969fe728515580e3150b
mehmetalikandemir <mehmet.kandemir@prettylittleting.com>
2023-05-07 21:07:49 +0300

    Created Environment Variable api key

--- src/http-common.ts
+++ src/http-common.ts
@@ -4,7 +4,7 @@ const apiClient: AxiosInstance =  axios.create({
     baseURL: "https://g1uv1ser4g.execute-api.us-west-1.amazonaws.com/dev",
     headers: {
         "Content-type": "application/json",
-        "x-api-key": "b8krXzbpdLbbA3SlWvldanNEslAMp6e2u3z3K1hh"
+        "x-api-key": import.meta.env.VITE_POST_X_API_KEY
     },
 });
 
