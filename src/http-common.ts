e637d93e5995d7c00f901183ba2ef90a186b7337
mehmetalikandemir <mehmet.kandemir@prettylittleting.com>
2023-05-07 21:10:59 +0300

    Created Environment Variable api url

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance =  axios.create({
-    baseURL: "https://g1uv1ser4g.execute-api.us-west-1.amazonaws.com/dev",
+    baseURL: import.meta.env.VITE_POST_URL,
     headers: {
         "Content-type": "application/json",
         "x-api-key": import.meta.env.VITE_POST_X_API_KEY
